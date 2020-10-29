31d3ec02c7a0f79e09bb6bf00a2f1c0b6c373eea
gbrsol <gabriel.n7899@hotmail.com>
2020-10-28 21:01:51 -0300

    update

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -8,11 +8,24 @@ module.exports = function(app)
     app.post('/forms/intervencao/salvar', function(req, res){
         var dados = req.body;
 
+        req.assert('nome','Nome é obrigatório').notEmpty();
+        req.assert('relax','Relaxamento é obrigatório').notEmpty();
+        req.assert('transition','Transição é obrigatório').notEmpty();
+        req.assert('clinical','Clínico é obrigatório').notEmpty();
+        req.assert('situation','Situação é obrigatório').notEmpty();
+
         var connection = app.config.dbConnection();
         var intervencaoModel = new app.app.models.IntervencaoDAO(connection);
         intervaoModel.insertIntervencao(intervencao, connection, function(error, result){
             res.redirect('/forms/intervencao');
         });
+
+        var erros = req.validationErrors();
+        if(erros)
+        {
+            res.render("admin/forms/intervencao");
+            return;
+        }
 
         res.send(dados);
     });
